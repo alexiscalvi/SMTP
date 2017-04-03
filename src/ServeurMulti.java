c4811d76b3c10c874d1db14a6e021abc43d70f91
AlexisCalvi <alex-goal73@hotmail.fr>
2017-04-03 09:10:29 +0200

    serv

--- src/ServeurMulti.java
+++ src/ServeurMulti.java
@@ -1,5 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  * Created by calvi on 03/04/2017.
  */
 public class ServeurMulti {
+
+    private ServeurSocket ss;
+
+    public void launch(int port) throws IOException {
+
+        this.ss = new ServeurSocket(port);
+         while(true){
+             Socket clientSocket = this.ss.accept();
+             System.out.println("new client " + clientSocket.getInetAddress());
+             ServeurThread thread = new ServeurThread(clientSocket);
+         }
+
+    }
+
 }
+
