02d7a720bb2d1499942059c328dfa791d6d69c5d
AlexisCalvi <alex-goal73@hotmail.fr>
2017-04-03 09:01:58 +0200

    src

---
+++ src/ServeurMulti.java
@@ -0,0 +1,5 @@
+/**
+ * Created by calvi on 03/04/2017.
+ */
+public class ServeurMulti {
+}
---
+++ src/ServeurSMTP.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+/**
+ * Created by calvi on 03/04/2017.
+ */
+public class ServeurSMTP {
+
+    public static void main(String[] args) throws IOException {
+
+        //instanciation du serveur
+        ServeurMulti serveurMulti = new ServeurMulti();
+        serveurMulti.launch(25);
+
+
+    }
+}
